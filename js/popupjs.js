5e2936490e607310069e1546e7b9159e98b17fe8
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-11-06 12:26:04 +0530

    Working Example of Remodle

--- js/popupjs.js
+++ js/popupjs.js
@@ -1,8 +1,28 @@
 (function($) {
     // Within this function, $ will always refer to jQuery
 
-$(document).ready(function(){
+$(document).on('open', '.remodal', function () {
+    console.log('open');
+});
 
-    
+$(document).on('opened', '.remodal', function () {
+    console.log('opened');
+});
+
+$(document).on('close', '.remodal', function () {
+    console.log('close');
+});
+
+$(document).on('closed', '.remodal', function () {
+    console.log('closed');
+});
+
+$(document).on('confirm', '.remodal', function () {
+    console.log('confirm');
+});
+
+$(document).on('cancel', '.remodal', function () {
+    console.log('cancel');
+});    
 
 })(jQuery);
